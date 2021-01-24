57692ae683ee5f189549a4ea4bff2878eb51bdd9
Rikuo1109 <Leanhduy11091412@gmail.com>
2021-01-24 17:53:17 +0700

    [DEV] Add response template
     -Create an response template
     -Create an sample API (list-user api not complete)

---
+++ abstract/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+
+class BaseSerializer(ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        fields = kwargs.pop('fields', None)
+        super(BaseSerializer, self).__init__(*args, **kwargs)
+        if fields is not None:
+            allowed = set(fields)
+            existing = set(self.fields)
+            for _field in existing - allowed:
+                self.fields.pop(_field)
+                
