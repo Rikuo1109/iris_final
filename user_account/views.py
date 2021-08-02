11c63b2a9c5cb1c9e9828da4c4f44ff8549f5b28
NgCuong <nguyen.cuong@horus.com.vn>
2021-08-02 22:15:34 +0700

    Add list user

--- user_account/views.py
+++ user_account/views.py
@@ -6,47 +6,66 @@ from utils.services import user as user_services, email as email_services
 from rest_framework import permissions, decorators, exceptions
 
 from django.urls import reverse
+from .models import User
+
 
 class AuthViewSet(viewset.BaseView):
-    permission_classes = [permissions.AllowAny, ]
+    permission_classes = [
+        permissions.AllowAny,
+    ]
     serializer_classes = {
-        'register': user_serializers.RegisterSerializer,
-        'active': user_serializers.ActiveSerializer,
-        'login': user_serializers.LoginSerializer,
-        'change_password': user_serializers.ChangePasswordSerializer,
-        'forgot_password': user_serializers.ForgotPasswordSerializer,
-        'reset_password': user_serializers.ResetPasswordSerializer,
+        "register": user_serializers.RegisterSerializer,
+        "active": user_serializers.ActiveSerializer,
+        "login": user_serializers.LoginSerializer,
+        "change_password": user_serializers.ChangePasswordSerializer,
+        "forgot_password": user_serializers.ForgotPasswordSerializer,
+        "reset_password": user_serializers.ResetPasswordSerializer,
     }
 
-    @decorators.action(methods=['GET', ], detail=False)
+    @decorators.action(methods=["GET"], url_path="list", detail=False)
+    def list_user(self, request):
+        try:
+            users = User.objects.all()
+            data = user_serializers.UserSerializer(users, many=True).data
+            return self.get_response(
+                data=viewset.paginate_data(request, data),
+                error_code=http_code.HttpSuccess,
+            )
+        except Exception as e:
+            return self.get_response(data=None, error_code=500)
+
+    @decorators.action(
+        methods=[
+            "GET",
+        ],
+        detail=False,
+    )
     def infor(self, request):
         try:
             user = request.user
             return self.get_response(
-                data = {
-                    'name': user.name,
-                    'is_admin': user.is_admin,
-                    'uid': user.uid
-                }, 
-                error_code=http_code.HttpSuccess
+                data={"name": user.name, "is_admin": user.is_admin, "uid": user.uid},
+                error_code=http_code.HttpSuccess,
             )
         except:
-            return self.get_response(
-                data = None, 
-                error_code=500
-            )
+            return self.get_response(data=None, error_code=500)
 
-    @decorators.action(methods=['POST', ], detail=False)
+    @decorators.action(
+        methods=[
+            "POST",
+        ],
+        detail=False,
+    )
     def register(self, request):
         """
-            A api to register a new user on the system
+        A api to register a new user on the system
 
-            :param request: email & password
+        :param request: email & password
 
-            @do:
-                -> Check valid email and password, If error, return response with status code 400 (error.status_code)
-                -> Create new user on the system, and send an email to active account
-                -> Return None, with error code is 0.
+        @do:
+            -> Check valid email and password, If error, return response with status code 400 (error.status_code)
+            -> Create new user on the system, and send an email to active account
+            -> Return None, with error code is 0.
         """
         serializer = self.get_serializer(data=request.data)
         try:
@@ -55,51 +74,64 @@ class AuthViewSet(viewset.BaseView):
             try:
                 from django.contrib.sites.shortcuts import get_current_site
 
-                email_services.active_email_sender(domain=get_current_site(request),email=user.email)
+                email_services.active_email_sender(
+                    domain=get_current_site(request), email=user.email
+                )
             except Exception as e:
                 print(e)
                 raise e
 
-            return self.get_response(
-                data=None,
-                error_code=http_code.HttpSuccess
-            )
+            return self.get_response(data=None, error_code=http_code.HttpSuccess)
         except exceptions.ValidationError as e:
             return self.get_response(data=e.detail, error_code=e.status_code)
 
-    @decorators.action(methods=['GET', ], detail=False)
+    @decorators.action(
+        methods=[
+            "GET",
+        ],
+        detail=False,
+    )
     def active(self, request):
         serializer = self.get_serializer(data=request.GET)
         try:
             serializer.is_valid(raise_exception=True)
-            is_active = user_services.active_user_account(serializer.validated_data['token'])
+            is_active = user_services.active_user_account(
+                serializer.validated_data["token"]
+            )
             if is_active:
                 return self.get_response(data=None, error_code=http_code.HttpSuccess)
-            return self.get_response(data={'User':['Cannot active account']}, error_code=http_code.HttpSomethingWentWrong)
-        except exceptions.ValidationError as e:
             return self.get_response(
-                data=e.detail,
-                error_code=e.status_code
+                data={"User": ["Cannot active account"]},
+                error_code=http_code.HttpSomethingWentWrong,
             )
+        except exceptions.ValidationError as e:
+            return self.get_response(data=e.detail, error_code=e.status_code)
 
-    @decorators.action(methods=['POST', ], detail=False)
+    @decorators.action(
+        methods=[
+            "POST",
+        ],
+        detail=False,
+    )
     def login(self, request):
         serializer = self.get_serializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
             user = user_services.get_and_authenticate_user(**serializer.validated_data)
             data = user_serializers.AuthSerializer(user).data
-            return self.get_response(
-                data = data,
-                error_code=http_code.HttpSuccess
-            )
+            return self.get_response(data=data, error_code=http_code.HttpSuccess)
         except exceptions.ValidationError as e:
-            return self.get_response(
-                data = e.detail,
-                error_code=e.status_code
-            )
+            return self.get_response(data=e.detail, error_code=e.status_code)
 
-    @decorators.action(methods=['GET', ],detail=False, permission_classes=[permissions.IsAuthenticated, ])
+    @decorators.action(
+        methods=[
+            "GET",
+        ],
+        detail=False,
+        permission_classes=[
+            permissions.IsAuthenticated,
+        ],
+    )
     def logout(self, request):
         data, is_complete = user_services.logout_user_account(request.GET)
         if is_complete:
@@ -107,36 +139,55 @@ class AuthViewSet(viewset.BaseView):
             return self.get_response(data=None, error_code=http_code.HttpSuccess)
         return self.get_response(data=data, error_code=http_code.HttpSomethingWentWrong)
 
-    @decorators.action(methods=['POST', ], detail=False, permission_classes=[permissions.IsAuthenticated, ])
+    @decorators.action(
+        methods=[
+            "POST",
+        ],
+        detail=False,
+        permission_classes=[
+            permissions.IsAuthenticated,
+        ],
+    )
     def change_password(self, request):
         serializer = self.get_serializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
-            user = user_services.get_and_authenticate_user(email=request.user.email, password= serializer.validated_data['current_password'])
-            user_services.change_user_password(user, serializer.validated_data['new_password'])
+            user = user_services.get_and_authenticate_user(
+                email=request.user.email,
+                password=serializer.validated_data["current_password"],
+            )
+            user_services.change_user_password(
+                user, serializer.validated_data["new_password"]
+            )
             return self.get_response(data=None, error_code=http_code.HttpSuccess)
         except exceptions.ValidationError as e:
-            return self.get_response(
-                data = e.detail,
-                error_code=e.status_code
-            )
+            return self.get_response(data=e.detail, error_code=e.status_code)
 
-    @decorators.action(methods=['GET', ], detail=False)
+    @decorators.action(
+        methods=[
+            "GET",
+        ],
+        detail=False,
+    )
     def forgot_password(self, request):
         serializer = self.get_serializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
             from django.contrib.sites.shortcuts import get_current_site
 
-            email_services.forgot_password_email_sender(domain=get_current_site(request), **serializer.validated_data)
+            email_services.forgot_password_email_sender(
+                domain=get_current_site(request), **serializer.validated_data
+            )
             return self.get_response(data=None, error_code=http_code.HttpSuccess)
         except exceptions.ValidationError as e:
-            return self.get_response(
-                data = e.detail,
-                error_code=e.status_code
-            )
+            return self.get_response(data=e.detail, error_code=e.status_code)
 
-    @decorators.action(methods=['POST', ], detail=False)
+    @decorators.action(
+        methods=[
+            "POST",
+        ],
+        detail=False,
+    )
     def reset_password(self, request):
         serializer = self.get_serializer(data=request.data)
         try:
@@ -144,7 +195,4 @@ class AuthViewSet(viewset.BaseView):
             user_services.reset_user_password(**serializer.validated_data)
             return self.get_response(data=None, error_code=http_code.HttpSuccess)
         except exceptions.ValidationError as e:
-            return self.get_response(
-                data = e.detail,
-                error_code=e.status_code
-            )
+            return self.get_response(data=e.detail, error_code=e.status_code)
